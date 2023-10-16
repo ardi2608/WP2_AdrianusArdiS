4e6007bf101b6e9e511b00b8ec6d14299717646d
AdrianusArdiS <ardimualang@gmail.com>
2023-10-16 21:53:48 +0700

    menambah file contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+
+<?php
+class contoh1 extends CI_Controller
+{ public function index()
+    {
+        echo "<h1?> perkenalkan </h1>";
+        echo "Nama saya ADRIANUS ARDI SAPUTRA Saya tinggal di bekasi 
+        Bermain musik adalah hobi saya";
+    }
+}
