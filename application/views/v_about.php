3859b7ff3be84e55da2482ef1fa474f44bd596f6
AdrianusArdiS <ardimualang@gmail.com>
2023-10-20 01:03:52 +0700

    menambahkan file

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : ADRIANUS ARDI</li>
+        <li>Nama Belakang : SAPUTRA</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Prima harapan regency</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulu tangkis</li>
+        <li>Renang</li>
+    </ul>
+</section>
